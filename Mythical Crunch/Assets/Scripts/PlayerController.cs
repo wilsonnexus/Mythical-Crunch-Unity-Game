33d88b3ff5ea4a1058d4d0a45a7bc9932403ea30
wilsonnexus <96637419+wilsonnexus@users.noreply.github.com>
2023-09-03 19:01:56 -0400

    Game Update

--- Mythical Crunch/Assets/Scripts/PlayerController.cs
+++ Mythical Crunch/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour
 {
@@ -19,15 +20,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        myRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed * Time.deltaTime / transform.localScale.x;
+        // Keyboard
+        // myRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed * Time.fixedDeltaTime / transform.localScale.x;
+        myRB.velocity = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")).normalized * speed * Time.fixedDeltaTime / transform.localScale.x;
 
         myAnim.SetFloat("moveX", myRB.velocity.x);
         myAnim.SetFloat("moveY", myRB.velocity.y);
 
-        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        // Keyboard
+        /*if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
         {
             myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
             myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }*/
+        if (CrossPlatformInputManager.GetAxis("Horizontal") == 1 || CrossPlatformInputManager.GetAxis("Horizontal") == -1 || CrossPlatformInputManager.GetAxis("Vertical") == 1 || CrossPlatformInputManager.GetAxis("Vertical") == -1)
+        {
+            myAnim.SetFloat("lastMoveX", CrossPlatformInputManager.GetAxis("Horizontal"));
+            myAnim.SetFloat("lastMoveY", CrossPlatformInputManager.GetAxis("Vertical"));
         }
     }
 }
